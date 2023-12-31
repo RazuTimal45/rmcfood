86e56b4a3e003fee0beeed1d495950e49f2b816b
RazuTimal45 <106792332+RazuTimal45@users.noreply.github.com>
2023-12-31 14:23:51 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,26 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
  var timeoutId;
 
  // Use mouseover and mouseleave on the productLink and productHover
+ if(document.querySelector('.navs-products[href="products.html"]'){
  productLink.addEventListener("mouseover", function () {
      clearTimeout(timeoutId);
      productHover.style.display = 'block';
  });
-
+ }
+  if(document.querySelector('.navs-products[href="products.html"]'){
  productLink.addEventListener("mouseleave", function () {
      timeoutId = setTimeout(function () {
          productHover.style.display = 'none';
      }, 1100);
  });
-
+  }
+  if(document.querySelector('.navs-products[href="products.html"]'){
  productHover.addEventListener("mouseover", function () {
      clearTimeout(timeoutId);
  });
-
+  }
+ if(document.querySelector('.navs-products[href="products.html"]'){
  productHover.addEventListener("mouseleave", function () {
      timeoutId = setTimeout(function () {
          productHover.style.display = 'none';
      }, 1100);
  });
+ }
  window.addEventListener('scroll', function() {
  // Check the scroll position or any other condition you need
  // For example, let's assume you want to hide productHover when the user scrolls down by 200 pixels
